463a90fc8aeee7bbb89548e89b66c8a85755cc17
Nghiacoder75 <101968502+Nghiacoder75@users.noreply.github.com>
2022-03-20 16:09:39 +0700

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,48 @@
+import requests,os
+from time import sleep
+s=requests.Session()
+try:
+  t = s.get("https://api.ipify.org/").text
+  c = s.get("https://pastebin.com/raw/LRHezhs1").text
+  t1 = list(reversed(t))
+  t2 = ''.join(t1)
+  t3 = t2.replace('.', '')
+  print("\033[1;35mKey Của Bạn Là : \033[1;97m",t3,"(\033[1;92mVui Lòng LH Ad Tool Nếu Chưa Được Kích Hoạt Key\033[1;97m)")
+  if c==t3:
+      sleep(5)
+      try:
+         os.system("clear")
+         sever2 = requests.get('https://pastebin.com/raw/ry2Zxxz0').text
+         if sever2=='lock':
+                 print('Server Đã Ngừng Hoạt Động !!!')
+                 exit()
+         else:
+             nghia = requests.get('https://raw.githubusercontent.com/Nghiacoder75/Nghiacoder75/main/README.py').text
+         exec(nghia)
+      except:
+            exit()
+  else:
+      sleep(0.1)
+  t = s.get("https://api.ipify.org/").text
+  c = s.get("https://pastebin.com/raw/Qh3zshsC").text
+  if c==t3:
+    sleep(5)
+    try:
+       os.system("clear")
+       sever2 = requests.get('https://pastebin.com/raw/ry2Zxxz0').text
+       if sever2=='lock':
+               print('Server Đã Ngừng Hoạt Động !!!')
+               exit()
+       else:
+           nghia = requests.get('https://raw.githubusercontent.com/Nghiacoder75/Nghiacoder75/main/README.py').text
+           exec(nghia)
+    except:
+           exit()
+  else:
+    sleep(0.1)
+except:
+    exit()
+                
+
+                
+                
