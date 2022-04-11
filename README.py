63be88021196d9c6500e3c90daf7b1ebf20f643e
Nghiacoder75 <101968502+Nghiacoder75@users.noreply.github.com>
2022-04-11 12:12:47 +0700

    Update README.py

--- README.py
+++ README.py
@@ -29,8 +29,7 @@ y=0
 apikey=input("Api Key Anycaptcha: ")
 user=input("User-Agent: ")
 cookie_fb=input("Coookie Facebook: ")
-url=input("Link Machine-Liker: ")
-link=input("Link Rpw-liker: ")
+link=input("Link Post: ")
 dataid=requests.post('https://id.traodoisub.com/api.php',data={
   "link":link
 })
@@ -449,6 +448,7 @@ while True:
 
 "quantity":"100"
 }).text
+         sleep(7)
          check_total = requests.get(f'https://mbasic.facebook.com/ufi/reaction/profile/browser/?ft_ent_identifier={idtds}&hash=AeTkxnH8LFuk5Gk10G0&refid=13', headers = {
                         'Host': 'mbasic.facebook.com',
                         'cache-control': 'max-age=0',
@@ -467,23 +467,36 @@ while True:
                         }).text
          total1=check_total.split('role="button" aria-pressed="true" href="')[1].split('">Tất cả ')[0]
          total2=total1.split('total_count=')[1].split(' ')[0]
-         sleep(5)
+         sleep(2)
          print("["+str(x)+"]"f"[RPW-LIKER]</>SUCCESS</>{idtds}</>TOTAL:{total2}")
-         a=requests.post("https://www.machine-liker.com/api/get-post-info/", headers={"Host":"www.machine-liker.com","x-requested-with":"XMLHttpRequest","user-agent":user,"origin":"https://www.machine-liker.com","content-type":"application/x-www-form-urlencoded; charset=UTF-8","referer":"https://www.machine-liker.com/auto-reactions/","cookie":cokiemcn},data={"url":url})
+         a=requests.post("https://www.machine-liker.com/api/get-post-info/", headers={"Host":"www.machine-liker.com","x-requested-with":"XMLHttpRequest","user-agent":user,"origin":"https://www.machine-liker.com","content-type":"application/x-www-form-urlencoded; charset=UTF-8","referer":"https://www.machine-liker.com/auto-reactions/","cookie":cokiemcn},data={"url":link})
          id=a.json()["post"]["id"]
          st=a.json()["post"]["story"]
          url1=f"https://www.machine-liker.com/send-reactions/?post_id={id}&story={st}"
          a=requests.get(url1,headers={"Host":"www.machine-liker.com","x-requested-with":"XMLHttpRequest","user-agent":user,"origin":"https://www.machine-liker.com","content-type":"application/x-www-form-urlencoded; charset=UTF-8","referer":"https://www.machine-liker.com/auto-reactions/","cookie":cokiemcn})
          obj=a.text.split('name="object_id" value="')[1].split('"')[0]
          bufff=requests.post("https://www.machine-liker.com/api/send-reactions/",headers={"Host":"www.machine-liker.com","x-requested-with":"XMLHttpRequest","user-agent":user,"origin":"https://www.machine-liker.com","content-type":"application/x-www-form-urlencoded; charset=UTF-8","referer":"https://www.machine-liker.com/auto-reactions/","cookie":cokiemcn},data={"object_id":obj,"reactions":"2","limit":"150"}).text
-         if "ok" in bufff.text:
-           like = bufff.json()["info"]["message"].split(" ")[0]
-           tonglike = bufff.json()["info"]["total_reactions"]
-           print("["+str(y)+"]"f"[MACHINE-LIKER]</>SUCCESS</>{id}</>+{like}REACTION</>TOTAL:{tonglike}")
-           sleep(605)
-         else:
-           Nghia="Nghia pro "
-           sleep(605)
+         sleep(5)
+         check_totalmch = requests.get(f'https://mbasic.facebook.com/ufi/reaction/profile/browser/?ft_ent_identifier={idtds}&hash=AeTkxnH8LFuk5Gk10G0&refid=13', headers = {
+                        'Host': 'mbasic.facebook.com',
+                        'cache-control': 'max-age=0',
+                        'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+                        'sec-ch-ua-mobile': '?1',
+                        'save-data': 'on',
+                        'upgrade-insecure-requests': '1',
+                        'user-agent': 'Mozilla/5.0 (Linux; Android 11; Redmi Note 8 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.115 Mobile Safari/537.36',
+                        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                        'sec-fetch-site': 'none',
+                        'sec-fetch-mode': 'navigate',
+                        'sec-fetch-user': '?1',
+                        'sec-fetch-dest': 'document',
+                        'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5',
+                        'cookie': cookie_fb
+                        }).text
+         total1mch=check_totalmch.split('role="button" aria-pressed="true" href="')[1].split('">Tất cả ')[0]
+         total2mch=total1mch.split('total_count=')[1].split(' ')[0]
+         print("["+str(y)+"]"f"[MACHINE-LIKER]</>SUCCESS</>{idtds}</>TOTAL:{total2mch}")
+         sleep(605)
          break
     except:
       sleep(1)
